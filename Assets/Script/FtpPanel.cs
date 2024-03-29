42c7089e8d2b9ed78f25d42fa65cab056fa85d58
Kaplc <755536175@qq.com>
2024-03-29 16:00:32 +0800

    完成Ftp获取文件大小，文件列表，创建文件夹

--- Assets/Script/FtpPanel.cs
+++ Assets/Script/FtpPanel.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Script.NetworkManager;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class FtpPanel : MonoBehaviour
 {
     public Button button;
-    public Button downloadButton;
+    public Button btnDownload;
+    public Button btnGetFileSize;
+    public Button btnGetFileList;
+    public Button btnCreateDirectory;
     
     private NetworkManager networkManager;
     
@@ -25,13 +29,40 @@ public class FtpPanel : MonoBehaviour
             });
         });
         
-        downloadButton.onClick.AddListener(() =>
+        btnDownload.onClick.AddListener(() =>
         {
             networkManager.ftpManager.DownloadFile("12.jpg", Application.dataPath + "/3.jpg", () =>
             {
                 Debug.Log("download success");
             });
             
+        });
+        
+        btnGetFileSize.onClick.AddListener(() =>
+        {
+            networkManager.ftpManager.GetFileSize("12.jpg", size =>
+            {
+                Debug.Log("size: " + size);
+            });
+        });
+        
+        btnCreateDirectory.onClick.AddListener(() =>
+        {
+            networkManager.ftpManager.CreateDirectory("Save2", () =>
+            {
+                Debug.Log("create directory success");
+            });
+        });
+        
+        btnGetFileList.onClick.AddListener(() =>
+        {
+            networkManager.ftpManager.GetFileList("Save2", list =>
+            {
+                foreach (var item in list)
+                {
+                    Debug.Log(item);
+                }
+            });
         });
     }
 }
